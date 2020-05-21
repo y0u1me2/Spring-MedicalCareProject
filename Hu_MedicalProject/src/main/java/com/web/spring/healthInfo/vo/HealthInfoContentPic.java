969ffa02cb505a45caf5f4b269e42bffc6cb8119
Park <Park@MSI>
2020-05-21 09:19:09 +0900

    지수 #18 건강정보 작성 구현중

---
+++ Hu_MedicalProject/src/main/java/com/web/spring/healthInfo/vo/HealthInfoContentPic.java
@@ -0,0 +1,16 @@
+package com.web.spring.healthInfo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HealthInfoContentPic {
+
+	private String healthInfoContent;
+	private String healthInfoNo;
+	private String healthInfoContentPic;
+	
+}
