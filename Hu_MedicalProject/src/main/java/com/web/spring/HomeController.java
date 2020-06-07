2012784ccda31c75275eb2679eba8199da06e8e6
이준호 <ljuho@naver.com>
2020-06-07 05:17:49 +0000

    주석 테스트

--- Hu_MedicalProject/src/main/java/com/web/spring/HomeController.java
+++ Hu_MedicalProject/src/main/java/com/web/spring/HomeController.java
@@ -32,6 +32,8 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+        
+        //주석
 		
 		return "home";
 	}
